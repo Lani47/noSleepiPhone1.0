a459bd4fd68b05eadd0811f3f089ffc6ed25c72b
Lani47 <21cm0147@jec.ac.jp>
2022-09-06 11:33:00 +0900

    Initial Commit

---
+++ noSleepiPhone/noSleepiPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  noSleepiPhoneApp.swift
+//  noSleepiPhone
+//
+//  Created by cmStudent on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct noSleepiPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
